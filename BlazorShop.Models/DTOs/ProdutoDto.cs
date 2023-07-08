b2e7f5609315d6078d9104f554185de4f67a22b5
JOESTAR117 <juniorrios1231@gmail.com>
2023-07-08 15:00:48 -0300

    feat: DTOs criados

---
+++ BlazorShop.Models/DTOs/ProdutoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorShop.Models.DTOs
+{
+    public class ProdutoDto
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Descricao { get; set; }
+        public required string ImagemUrl { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public string? CategoriaNome { get; set; }
+    }
+}
