bf76e09ea4407b30c6c0d6f34ebfa5a5d12fe678
JOESTAR117 <juniorrios1231@gmail.com>
2023-07-08 19:04:22 -0300

    feat: Repositories Carrinho Compra criados

---
+++ BlazorShop.API/Repositories/CarrinhoCompraRepository.cs
@@ -0,0 +1,41 @@
+﻿using BlazorShop.API.Data;
+using BlazorShop.API.Entities;
+using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.API.Repositories
+{
+    public class CarrinhoCompraRepository : ICarrinhoCompraRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarrinhoCompraRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<CarrinhoItem> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> AtualizaQuantidade(int id, CarrinhoItemAtualizaQuantidadeDto carrinhoItemAtualizaQuantidadeDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> DeletaItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CarrinhoItem>> GetItens(string usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
