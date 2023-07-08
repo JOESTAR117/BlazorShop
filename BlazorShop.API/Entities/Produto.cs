03f386d9dad1e55cee3dffcd7a00c089349a6369
JOESTAR117 <juniorrios1231@gmail.com>
2023-07-08 14:22:11 -0300

    feat: Entidades criadas e banco de dados conectado

---
+++ BlazorShop.API/Entities/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorShop.API.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Descricao { get; set; }
+        public required string ImagemUrl { get; set; }
+        public required decimal Preco { get; set; }
+        public required int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+
+        public ICollection<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
+    }
+}
---
+++ BlazorShop.API/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorShop.API.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string NomeUsuario { get; set; }
+
+        public Carrinho? Carrinho { get; set; }
+    }
+}
